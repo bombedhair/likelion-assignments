155293951eb966e5a0005356f8f92fc2cc60427c
YoungRok Kim <bombedhair@gmail.com>
2022-06-02 23:43:54 +0900

    Revert "6/2 assignment"
    This reverts commit 1b8bae6e2f309f237394f4a4f28ddc5f838f4aef.

--- 6_2/community/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class WriteForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'image']
--- 6_2/community/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.create, name='create'),
-    path('delete/<int:article_id>/', views.delete, name='delete'),
-    path('update/<int:article_id>/', views.update, name='update')
-]
